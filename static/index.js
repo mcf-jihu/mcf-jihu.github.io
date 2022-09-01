375c750aef3e6db814219c737027faaf011955e2
mc_f <112634119+mcf-jihu@users.noreply.github.com>
2022-09-01 21:39:54 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -177,7 +177,7 @@ function SubmitResults() {
 function gameTime() {
     _gameTimeNum--;
     if (_gameTimeNum <= 0) {
-        GameTimeLayer.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;时间到！';
+        GameTimeLayer.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;踩保翔的时间到了！';
         gameOver();
         GameLayerBG.className += ' flash';
         createjs.Sound.play("end");
@@ -343,11 +343,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 30) return '鸡你太美';
-    if (score <= 60) return '鸡你太太美';
-    if (score <= 100) return '鸡鸡鸡鸡鸡';
-    if (score <= 150) return '唱 跳 rap 篮球';
-    return '你就是蔡徐坤本人？';
+    if (score <= 30) return '王保翔说你太逊了，还不到30';
+    if (score <= 60) return '王保翔说你还可以';
+    if (score <= 100) return '王保翔说你太棒了';
+    if (score <= 150) return '王保翔？';
+    return '你就是王保翔本人？';
 }
 
 function toStr(obj) {
